91fcd2865db5f4e8fdda3387b307532f712b3e26
Gilbert <nicg771@gmail.com>
2026-09-17 14:35:16 +0700

    feat: add Nunito custom font

--- android/app/src/main/java/id/tinyspoon/app/ui/theme/Type.kt
+++ android/app/src/main/java/id/tinyspoon/app/ui/theme/Type.kt
@@ -2,33 +2,45 @@ package id.tinyspoon.app.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
+import id.tinyspoon.app.R
 
-// Set of Material typography styles to start with
+val NunitoFontFamily = FontFamily(
+    Font(R.font.nunito_regular, FontWeight.Normal),
+    Font(R.font.nunito_medium, FontWeight.Medium),
+    Font(R.font.nunito_bold, FontWeight.Bold),
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NunitoFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = NunitoFontFamily,
+        fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NunitoFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
     )
-    */
+)
+
+fun nunitoStyle(
+    fontSize: TextUnit,
+    fontWeight: FontWeight = FontWeight.Normal
+) = TextStyle(
+    fontFamily = NunitoFontFamily,
+    fontSize = fontSize,
+    fontWeight = fontWeight
 )
